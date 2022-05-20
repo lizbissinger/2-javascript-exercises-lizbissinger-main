bb79a4ef3c1f0886346b74b4c132e775d8699385
lizbissinger <105609716+lizbissinger@users.noreply.github.com>
2022-05-19 21:30:22 -0400

    completed numbers add and return two decimals

--- src/1.numbers.js
+++ src/1.numbers.js
@@ -24,13 +24,16 @@ function multiply(num1, num2) {
 
 //parseInt: should use parseInt correctly to convert strings to integer numbers
 function parseInt (str) {
-  return ;
+  return;
 }
 
 //addAndReturn2DecimalPlaces: should return a number value, rounded to 2 decimal places
 // Example: 1.23453 + 5.37873 should return 6.61 and the return type should be a number
 function addAndReturn2DecimalPlaces(num1, num2) {
-  return ;
+
+  constresult = Number((num1 + num2).toFixed(2));
+
+  return constresult;
 }
 
 module.exports  = {
