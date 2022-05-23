3e3e31f1b5f685e2df29acba482f36fc95af5286
lizbissinger <105609716+lizbissinger@users.noreply.github.com>
2022-05-22 22:15:24 -0400

    completed logical perators

--- src/5.logicalOperators.js
+++ src/5.logicalOperators.js
@@ -2,13 +2,21 @@
 
 // or: should return true if either a or b are truthy. False, if both are false
 function or(a, b) {
-  return ;
+  if(a||b){
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+  
 }
 
 //and: should return true only if both a and b are truthy
 function and(a, b) {
-  return ;
+  return a==true&&b==true;
 }
+console.log(and(true,false))
 
 module.exports = {
   or,
