650ae7fca83b5ef4cd2eea6c186b0f0dc4cf0403
lizbissinger <105609716+lizbissinger@users.noreply.github.com>
2022-05-22 22:29:32 -0400

    completed equality

--- src/7.equality.js
+++ src/7.equality.js
@@ -2,15 +2,20 @@
 // Example: strictEquality(1, true) => false
 // Example: strictEquality(1, 1) => true
 function strictEquality(val1, val2) {
-  return ;
+
+  return val1===val2;
 }
+
+
 //equality: compare value but not necessarily the type
 // Example: equality(1, true) => true
 // Example: equality(1, 1) => true
 // Example: equality(1, false) => false
 function equality(val1, val2) {
-  return ;
+
+  return val1==val2;
 }
+
 
 module.exports = {
   strictEquality,
